576bb45857dcc52cc7750baf395ccdfa18b064c6
Will <whof@bu.edu>
2019-06-28 18:29:39 -0400

    added survey questions

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,3 @@
+//get route with URL /api/friends to display json object of all possible friends
+
+//post route to /api/friends to handle incoming survey results. route will also handle compatibility logic.
